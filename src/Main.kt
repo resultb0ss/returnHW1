6d5880dc076c13006475f803067ce342d9bce573
Admin <reteamboss@gmail.com>
2024-08-26 17:24:42 +0300

    Задание 1 RETURN

---
+++ src/Main.kt
@@ -0,0 +1,18 @@
+fun main() {
+
+    val array = arrayOf(5,2,7,6,8,3,4)
+    var result = checkLocal(array)
+    println("Локальных минимумов в массиве: $result")
+}
+
+fun checkLocal (array: Array<Int>): Int {
+    var countLocMin = 0
+    for ( i in array.indices){
+        if (i != 0 && i != array.size - 1 ){
+            if (array[i] < array[i+1] && array[i] < array[i-1]){
+                countLocMin++
+            }
+        }
+    }
+    return countLocMin
+}
